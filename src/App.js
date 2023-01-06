980c8c0bb88a4682b51cc059e3aaa54c13014871
Aleksandar Radev <alexander.radev.96@gmail.com>
2023-01-06 17:49:10 +0200

    add firebase database to project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
 import { Container, Graphics, Text } from "pixi.js";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, onValue } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMS1UbAUbXbyalJh4argrqWYSgg2Rrkqg",
+  authDomain: "animal-idle-v1.firebaseapp.com",
+  databaseURL: "https://animal-idle-v1-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "animal-idle-v1",
+  storageBucket: "animal-idle-v1.appspot.com",
+  messagingSenderId: "79194368784",
+  appId: "1:79194368784:web:ae58a4ca85c979d7fd5395",
+  measurementId: "G-L06KLWGC80"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+const starCountRef = ref(database, 'totalVisits');
+onValue(starCountRef, (snapshot) => {
+  const data = snapshot.val();
+  console.log(data)
+});
+
 
 function ellipse(size, color) {
   const ratio = 2.5;
