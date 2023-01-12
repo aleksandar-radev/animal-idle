653d4be28b602b1aea8af98f9c9a72b33afcabda
Aleksandar Radev <alexander.radev.96@gmail.com>
2023-01-12 19:13:59 +0200

    change project to Vite, for performance

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,16 +1,16 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Context } from '../Store';
+import React, { useEffect, useState } from 'react';
+// import { Context } from '../Store';
 // import Random2 from "./Random2";
-import { ref, onValue, set, getDatabase } from "firebase/database";
+import { ref, onValue, set } from "firebase/database";
 import database from "../api/Api";
 
 const Random = () => {
-    const [state, setState] = useContext(Context)
-    const [clicks, setClicks] = useState(1)
+    // const [state, setState] = useContext(Context)
+    const [clicks, setClicks] = useState(undefined)
 
-    const starCountRef = ref(database(), 'totalVisits');
-
+    
     useEffect(() => {
+        const starCountRef = ref(database(), 'totalVisits');
         onValue(starCountRef, (snapshot) => {
             const data = snapshot.val();
             setClicks(data)
@@ -19,19 +19,16 @@ const Random = () => {
     }, []);
 
     const addCount = () => {
-        console.log('asdasd')
+        console.log('asdasds')
+        if (!clicks) return;
         set(ref(database(), 'totalVisits'), clicks + 1);
 
     }
 
     return (
         <>
-            <h2>im a cool components</h2>
             <p onClick={addCount}>total clicks: {clicks}</p>
-
-            {/* <Random2></Random2> */}
         </>
-
     )
 }
 
