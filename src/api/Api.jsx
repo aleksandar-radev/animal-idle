dae47a72fbee6587ee450e1c4b4b81ff66a222f0
Aleksandar Radev <alexander.radev.96@gmail.com>
2023-02-20 21:26:54 +0200

    remove firebase config from codebase, extract to env

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -4,14 +4,14 @@ import { getDatabase } from 'firebase/database';
 import { ref, onValue } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAMS1UbAUbXbyalJh4argrqWYSgg2Rrkqg',
-  authDomain: 'animal-idle-v1.firebaseapp.com',
-  databaseURL: 'https://animal-idle-v1-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'animal-idle-v1',
-  storageBucket: 'animal-idle-v1.appspot.com',
-  messagingSenderId: '79194368784',
-  appId: '1:79194368784:web:ae58a4ca85c979d7fd5395',
-  measurementId: 'G-L06KLWGC80',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
