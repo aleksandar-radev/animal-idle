45e93cfe8f74457497c4ddfa616294939f3608c8
Aleksandar Radev <alexander.radev.96@gmail.com>
2023-03-23 03:13:44 +0200

    fix health bar style

--- src/components/HealthBar.jsx
+++ src/components/HealthBar.jsx
@@ -3,7 +3,14 @@ import PropTypes from '../externalLibraries/propTypes';
 import './HealthBar.scss';
 
 const HealthBar = (props) => {
-  return <div className={'HealthBar'}>{`${props.currentHealth} / ${props.totalHealth}`}</div>;
+  const percentage = (props.currentHealth / props.totalHealth) * 100;
+  return (
+    <div
+      style={{
+        backgroundImage: `linear-gradient(to right, hsl(calc(${percentage} * 1.2), 100%, 50%)  ${percentage}%, white ${percentage}%)`,
+      }}
+      className={'HealthBar'}>{`${props.currentHealth} / ${props.totalHealth}`}</div>
+  );
 };
 
 HealthBar.propTypes = {
