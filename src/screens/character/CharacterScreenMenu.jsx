12d887f63fa59e26ace46319fb0be1bca4ea9005
Aleksandar Radev <alexander.radev.96@gmail.com>
2023-09-06 15:11:06 +0300

    add color to active tab

--- src/screens/character/CharacterScreenMenu.jsx
+++ src/screens/character/CharacterScreenMenu.jsx
@@ -14,11 +14,27 @@ const CharacterScreenMenu = () => {
     store.tabs.setActiveCharacterScreenTab(view);
   };
 
+  const isActiveTab = (tab) => {
+    return store?.tabs?.activeCharacterScreenTab === tab;
+  };
+
   return (
     <div className={'CharacterScreenMenu'}>
-      <div onClick={() => changeView(CHARACTER_SCREEN_STATS_TAB)}>Stats</div>
-      <div onClick={() => changeView(CHARACTER_SCREEN_SKILLS_TAB)}>Skills</div>
-      <div onClick={() => changeView(CHARACTER_SCREEN_ITEMS_TAB)}>Items</div>
+      <div
+        className={isActiveTab(CHARACTER_SCREEN_STATS_TAB) ? 'active' : ''}
+        onClick={() => changeView(CHARACTER_SCREEN_STATS_TAB)}>
+        Stats
+      </div>
+      <div
+        className={isActiveTab(CHARACTER_SCREEN_SKILLS_TAB) ? 'active' : ''}
+        onClick={() => changeView(CHARACTER_SCREEN_SKILLS_TAB)}>
+        Skills
+      </div>
+      <div
+        className={isActiveTab(CHARACTER_SCREEN_ITEMS_TAB) ? 'active' : ''}
+        onClick={() => changeView(CHARACTER_SCREEN_ITEMS_TAB)}>
+        Items
+      </div>
     </div>
   );
 };
